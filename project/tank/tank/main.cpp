ad7430d33dc64b0acb9f39ba51c06579f5cc8cad
chenlonglong <chenlonglong@qiyi.com>
2019-03-05 20:03:50 +0800

    add main.cpp

---
+++ project/tank/tank/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tank
+//
+//  Created by 陈龙 on 2019/2/3.
+//  Copyright © 2019 陈龙. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
